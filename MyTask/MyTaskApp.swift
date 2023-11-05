889016486da906df352834b5c708cfdbfa2c87f7
icp049 <ianpepe2000@gmail.com>
2023-11-04 21:42:23 -0600

    Initial Commit

---
+++ MyTask/MyTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTaskApp.swift
+//  MyTask
+//
+//  Created by Ian Pedeglorio on 2023-11-04.
+//
+
+import SwiftUI
+
+@main
+struct MyTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
